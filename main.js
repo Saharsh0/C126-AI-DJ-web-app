faea28e64d6c2287664eda5e254f367520e36f5d
Saharsh0 <113522933+Saharsh0@users.noreply.github.com>
2023-09-25 17:32:03 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ wristLY = 0;
 wristRX = 0;
 wristRY = 0;
 leftWristScore = 0;
+RightWristScore = 0;
 function preload() {
     song = loadSound("music.mp3");
 }
@@ -23,15 +24,51 @@ function draw() {
 
     fill("#FF0000");
     stroke("FF0000");
+    console.log(RightWristScore);
+
+
+        console.log("RIGHT WRIST IF CONDITION")
+        circle(wristRX, wristRY, 20)
+
+        if(wristRY <= 100)
+        {
+            document.getElementById("speed").innerHTML = "Speed = 0.5X";
+            song.rate(0.5);
+        }
+
+        else if(wristRY > 100 && wristRY <= 200)
+        {
+            document.getElementById("speed").innerHTML = "Speed = 1X";
+            song.rate(1);
+        }
+
+        else if(wristRY > 200 && wristRY <= 300)
+        {
+            document.getElementById("speed").innerHTML = "Speed = 1.5X";
+            song.rate(1.5);
+        }
+
+        else if(wristRY > 300 && wristRY <= 400)
+        {
+            document.getElementById("speed").innerHTML = "Speed = 2X";
+            song.rate(2);
+        }
+
+        else if(wristRY > 400)
+        {
+            document.getElementById("speed").innerHTML = "Speed = 2.5X";
+            song.rate(2.5);
+        }
+
 
     if (leftWristScore > 0.2) {
 
 
 
-        circle(leftWristX, leftWristY, 20)
+        circle(wristLX, wristLY, 20)
 
-        inNumberLeftWristY = Number(leftWristY);
-        removeDecimals = floor(inNumberLeftWristY)
+        inNumberLeftWristY = Number(wristLY);
+        removeDecimals = floor(inNumberLeftWristY);
         volume = removeDecimals / 500;
 
         document.getElementById("volume").innerHTML = "Volume = " + volume;
@@ -53,12 +90,13 @@ function gotPoses(results) {
     if (results.length > 0) {
         console.log(results);
 
-        leftWristScore = results[0].pose.keypoints[9].score
+        leftWristScore = results[0].pose.keypoints[9].score;
+        RightWristScore = results[0].pose.keypoints[10].score;
 
-        wristLX = results[0].pose.leftWrist.x
-        wristLY = results[0].pose.leftWrist.y
+        wristLX = results[0].pose.leftWrist.x;
+        wristLY = results[0].pose.leftWrist.y;
 
-        wristRX = results[0].pose.rightWrist.x
-        wristRY = results[0].pose.rightWrist.y
+        wristRX = results[0].pose.rightWrist.x;
+        wristRY = results[0].pose.rightWrist.y;
     }
 }
